fb70c6acba5dd90ab70654b829c4fafdb8ac62b7
Azide <rukuy@qq.com>
2023-06-18 02:07:35 +0800

    :bug: 更改网址

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,11 +11,11 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://blog.ceobecanteen.top",
+  url: "https://enraged-dun-cookie-development-team.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
-
+  baseUrl: "/Ceobe-Canteen-Blog/",
+  trailingSlash: false,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "Enraged-Dun-Cookie-Development-Team", // Usually your GitHub org/user name.
