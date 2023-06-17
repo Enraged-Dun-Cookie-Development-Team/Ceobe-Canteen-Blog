91df495588db902280cab078ac1a52359046c5c2
AzideCupric <57004769+AzideCupric@users.noreply.github.com>
2023-06-18 01:51:57 +0800

    添加github actions部署 (#1)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-test-site.com",
+  url: "https://blog.ceobecanteen.top",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -132,6 +132,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+        additionalLanguages: ["rust", "toml"],
       },
     }),
 };
